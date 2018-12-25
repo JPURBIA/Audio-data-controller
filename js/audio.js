e68abf696b9cb70eacd57e5a226be2b609f0ef21
jpurbia <jontypurbia.udr@gmail.com>
2018-12-25 23:12:10 +0530

    Everything except for load file functionality has been implemented.

--- js/audio.js
+++ js/audio.js
@@ -1,31 +1,41 @@
-var myAudio, endTime, curTime, myVol, myTrack;
+var myAudio, endTime, curTime, myVol, seeker;
 window.onload = function () {
     myAudio = document.getElementById('myAudio');
     endTime = document.getElementById('end');
     curTime = document.getElementById('cur');
     myVol = document.getElementById('myVol');
-    myTrack = document.getElementById('myTrack');
+    seeker = document.getElementById('seekObj');
     setDefaults();
     loadAudio();
-    console.log(myAudio);
+    // console.log(myAudio);
+}
+function initProgressBar() {
+    var curTimeString = calculateTimeString(myAudio.currentTime);
+    curTime.innerHTML = curTimeString;
+
+    seeker.value = (myAudio.currentTime / myAudio.duration);
+    seeker.addEventListener("click", seek);
+
+    function seek(evt) {
+        var percent = evt.offsetX / this.offsetWidth;
+        myAudio.currentTime = percent * myAudio.duration;
+        seeker.value = percent / 100;
+    }
 }
 function setDefaults() {
     myVol.value = 50;
     myAudio.volume = 0.5;
-    myTrack.value = 0;
 }
 function loadAudioFile() {
 
 }
-function loadAudio() { 
+function loadAudio() {
     myAudio.load();
-    myAudio.addEventListener("loadedmetadata", function(){
-        // console.log("Playing " + myAudio.src + " for: "+ myAudio.duration + " seconds.");
+    myAudio.addEventListener("loadedmetadata", function () {
         displayTime();
     });
 }
 function playAudio() {
-    // console.log("HelloWorld;")
     myAudio.play();
 }
 function pauseAudio() {
@@ -62,20 +72,19 @@ function displayTime() {
 }
 function calculateTimeString(tot_time) {
     var min = Math.floor(tot_time / 60);
-    var sec = tot_time - (min*60);
+    var sec = Math.floor(tot_time - (min * 60));
     var secs = sec.toString();
     var mins = min.toString();
-    if(secs.length >= 2)
+    // console.log(secs);
+    if (sec >= 10)
         secs = secs.substring(0, 2);
     else
-        secs += '0';
-    // console.log(mins, secs);
-    
-    return min + ':' + secs;
+        secs = '0' + secs;
+    return mins + ':' + secs;
 }
 function changeVolume() {
-    myAudio.volume = (myVol.value/100);
+    myAudio.volume = (myVol.value / 100);
 }
 function changePosition() {
-    myAudio.currentTime = (myTrack.value/100)*myAudio.duration;
+    myAudio.currentTime = (myTrack.value / 100) * myAudio.duration;
 }
